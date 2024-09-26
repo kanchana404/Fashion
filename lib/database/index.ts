cef2b62cf49b1bdedd67a422a8ad6a59c25aa89a
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-09-26 12:46:47 +0530

    fix: Update database name from 'forum' to 'Fashion' and add comment in user model

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'forum',
+    dbName: 'Fashion',
     bufferCommands: false,
   })
 console.log("connected to db");
